14cf6106e264c25aa52d724ebde23ca8921374af
Wouter Westenbrink <w.westenbrink@gmail.com>
2013-10-20 12:32:51 +0200

    use latest opsworks agent release (209)

--- provision/opsworks.sh
+++ provision/opsworks.sh
@@ -1,8 +1,8 @@
 # install ruby1.8 & dependencies
 apt-get install -y ruby ruby-dev rubygems libicu-dev libssl-dev libopenssl-ruby libxslt-dev libxml2-dev
 
-# download & extract opswork-agent-installer (release 208)
-wget -O /tmp/opsworks-agent.tgz https://opsworks-instance-agent.s3.amazonaws.com:443/208/opsworks-agent-installer.tgz
+# download & extract opswork-agent-installer (release 209)
+wget -O /tmp/opsworks-agent.tgz https://opsworks-instance-agent.s3.amazonaws.com:443/209/opsworks-agent-installer.tgz
 cd /tmp && tar -xvzpof opsworks-agent.tgz
 
 # build & install custom chef gem
