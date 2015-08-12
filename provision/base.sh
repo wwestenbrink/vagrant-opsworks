20fc13c315a14b81eaa3213ae004fa80a10bfed3
Pavlos Ratis <dastergon@dastergon.gr>
2015-08-12 14:00:54 +0300

    use add-apt-repositories to enable multiverse repo

--- provision/base.sh
+++ provision/base.sh
@@ -1,25 +1,9 @@
+# change the default frontend of debconf
+export DEBIAN_FRONTEND=noninteractive
+
 # enable multiverse repositories
-code_name=`lsb_release -c | awk '{ print $2 }'`
-  cat <<EOM > /etc/apt/sources.list.d/$code_name-multiverse.list
-  deb http://archive.ubuntu.com/ubuntu/ $code_name main universe
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name main universe
-  deb http://archive.ubuntu.com/ubuntu/ $code_name-updates main universe
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name-updates main universe
-  deb http://archive.ubuntu.com/ubuntu $code_name-security main universe
-  deb-src http://archive.ubuntu.com/ubuntu $code_name-security main universe
-  deb http://archive.ubuntu.com/ubuntu/ $code_name multiverse
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name multiverse
-  deb http://archive.ubuntu.com/ubuntu/ $code_name-updates multiverse
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name-updates multiverse
-  deb http://archive.ubuntu.com/ubuntu $code_name-security multiverse
-  deb-src http://archive.ubuntu.com/ubuntu $code_name-security multiverse
-  deb http://archive.ubuntu.com/ubuntu/ $code_name multiverse
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name multiverse
-  deb http://archive.ubuntu.com/ubuntu/ $code_name-updates multiverse
-  deb-src http://archive.ubuntu.com/ubuntu/ $code_name-updates multiverse
-  deb http://security.ubuntu.com/ubuntu $code_name-security multiverse
-  deb-src http://security.ubuntu.com/ubuntu $code_name-security multiverse
-EOM
+apt-get install -y software-properties-common
+add-apt-repository multiverse
 
 # update the package repository
 apt-get update
